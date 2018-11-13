1fc8b4bff493b33a07a704058f692d9b25945c18
megaton <lkh890111@gmail.com>
2018-11-13 18:58:22 +0900

    코드 정리 중

--- html/js/index.js
+++ html/js/index.js
@@ -2,44 +2,19 @@ import React , { Component } from 'react' ;
 import ReactDOM , { render } from 'react-dom' ;
 import { Common } from './ui/common' ;
 
-/*
-class A {
-    constructor () {
-        this.B = class {
-            echo () {
-                console.log('I am B class');
-            }
-        }
-    }
-    echo () {
-        this.b = new this.B;
-        this.b.echo();
-    }
-}
-
-var a = new A;
-
-a.echo();
-*/
-
-
-
-const SelectMenuCustomModule = ( opts ) => {
+const SelectMenuCustomModule = ( target, opts ) => {
 
 	class SelectMenuCustom {
-		constructor( selectTarget ){
+		constructor( target, opts ){
 
-			this._elem = {}, this._obj = {}, this._opt = {} ;
+			this._opts = opts || {} ;
 
-			this._elem.originSelect = selectTarget ,
-			this._elem.newWrapper, this._elem.newBox, this._elem.newBth  ;
-			this._obj.itemArr = [], this._obj.crntSelect ,
-			this._obj.sltOpt = this._elem.originSelect.querySelectorAll('option') ,
-			this._opt.prevIdx = 0, this._opt.activeClass = 'active' ;
+			this.originSelect = target, this.newWrapper, this.newElem, this.newBox, this.newBth, this.itemArr = [],	this.crntSelect ,
+			this.prevIdx = 0,	this.activeClass = 'active' ,
+			this.sltOpt = this.originSelect.querySelectorAll('option') ;
+			this._opts.dir = this._opts.dir || 'down' ;
 
-			console.log( 'this._elem.originSelect :', this._elem.originSelect ) ;
-
-			this._elem.originSelect.style.display = 'none' ;
+			this.originSelect.style.display = 'none' ;
 			this.init() ;
 
 		}
@@ -47,93 +22,162 @@ const SelectMenuCustomModule = ( opts ) => {
 
 			let _this = this ;
 
-			for( let i=0; i<this._obj.sltOpt.length; i++ ) {
-				if( this._obj.sltOpt[i].selected ) {
-					this._opt.prevIdx = i ;
+			for( let i=0; i<this.sltOpt.length; i++ ) {
+
+				if( this.sltOpt[i].selected ) {
+					this.prevIdx = i ;
 				}
-				this._obj.itemArr[i] = {
-					title : this._obj.sltOpt[i].firstChild.textContent ,
-					value : this._obj.sltOpt[i].value ,
-					selected : this._obj.sltOpt[i].selected ,
+
+				this.itemArr[i] = {
+					title : this.sltOpt[i].firstChild.textContent ,
+					value : this.sltOpt[i].value ,
+					selected : this.sltOpt[i].selected ,
 				}
+
 			}
 
-			let newSelectBox = this.makeNewSelectBox( this._obj.itemArr ) ;
-			this._elem.originSelect.parentNode.insertBefore( newSelectBox , this._elem.originSelect.nextSibling ) ;
+			let newSelectBox = this.makeNewSelectBox( this.itemArr ) ;
+			// this.originSelect.parentNode.insertBefore( newSelectBox , this.originSelect.nextSibling ) ;
+			this.originSelect.parentNode.insertAdjacentHTML( 'beforeend', newSelectBox ) ;
 
-			this._elem.originSelect.addEventListener('change', function() {
+			[].forEach.call( this.newBox.querySelectorAll('button'), (item, idx) =>{
+				item.addEventListener('click', () => this.selectItem( idx ) )
+			}) ;
+
+
+			this.originSelect.addEventListener('change', function() {
 				_this.selectItem( this.selectedIndex ) ;
 			}) ;
 
 		}
 
 		makeNewSelectBox( items ) {
-			console.log( 'makeNewSelectBox in', items ) ;
-			this._elem.newWrapper = document.createElement('div') ;
-			this._elem.newWrapper.classList.add('custom_select_box') ;
 
-			this._elem.newBox = document.createElement('div') ;
-			this._elem.newBtn = document.createElement('button') ;
-			this._elem.newBtn.classList.add('btn_select_box') ;
+			this.newWrapper = document.createElement('div') ;
+			this.newBox = document.createElement('div') ;
+			// this.newBox = document.createElement('div') ;
 
+			let wrapClass = this._opts.dir == 'up' ? ['custom_select_box', 'upShow'] : ['custom_select_box'] ;
 			let crntSelected = items.filter( item => item.selected ) ;
-			this._elem.newBtn.textContent = crntSelected.length > 0 ? crntSelected[0].title : '선택' ;
 
-			this._elem.newBtn.addEventListener( 'click', () => this.selectToggle() ) ;
+			this.newWrapper.classList.add( ...wrapClass ) ;
+
+
+			this.newBtn = document.createElement('button') ;
+			this.newBtn.classList.add('btn_select_box') ;
+			this.newBtn.textContent = crntSelected.length > 0 ? crntSelected[0].title : '선택' ;
+
+			// this.newBtn = `<button type="button" class="btn_select_box">${crntSelected.length > 0 ? crntSelected[0].title : '선택'}</button>` ;
+
+			// this.newBtn.addEventListener( 'click', () => this.selectToggle() ) ;
 
 			items.forEach(( item, idx ) => {
-				let subItem = document.createElement('button') ;
-				subItem.textContent = item.title ;
-				subItem.setAttribute('data-value', item.value ) ;
-				this._elem.newBox.appendChild( subItem ) ;
-
-				subItem.addEventListener('click', () => this.selectItem( idx ) ) ;
+				let subItem = `<button type="button" data-value="${item.value}">${item.title}</button>` ;
+				this.newBox.innerHTML += subItem ;
 			}) ;
 
-			this._elem.newWrapper.appendChild( this._elem.newBtn ) ;
-			this._elem.newWrapper.appendChild( this._elem.newBox ) ;
+			this.newWrapper.appendChild( this.newBtn ) ;
+			this.newWrapper.appendChild( this.newBox ) ;
+			// this.newWrapper.insertAdjacentHTML( 'afterbegin', this.newBtn ) ;
+			// this.newWrapper.insertAdjacentHTML( 'beforeend', this.newBox ) ;
 
-			return this._elem.newWrapper ;
+			this.newElem =
+			`<div class="custom_select_box upShow">
+				<button class="btn_select_box">${crntSelected.length > 0 ? crntSelected[0].title : '선택'}</button>
+					<div>
+						${this.makeOptionList( items )}
+					</div>
+			</div>` ;
 
+			console.log( 'this.newElem :', this.newElem );
+
+			return this.newElem ;
+			// return this.newWrapper ;
+		}
+
+		makeOptionList( items ){
+			let optionList = '' ;
+			items.forEach(( item, idx ) => {
+				optionList += `<button type="button" data-value="${item.value}">${item.title}</button>` ;
+			}) ;
+			return optionList;
 		}
 
 		selectToggle(){
 
-			this._elem.newWrapper.classList.contains( this._opt.activeClass ) ? this._elem.newWrapper.classList.remove( this._opt.activeClass ) : this._elem.newWrapper.classList.add( this._opt.activeClass ) ;
+			this.newWrapper.classList.contains( this.activeClass ) ? this.newWrapper.classList.remove( this.activeClass ) : this.newWrapper.classList.add( this.activeClass ) ;
 
 		}
 
 		selectItem( crntIdx ){
 
-			this._obj.crntSelect = this._obj.itemArr[crntIdx] ;
-			this._elem.originSelect.selectedIndex = crntIdx ;
-			this._obj.itemArr[this._opt.prevIdx].selected = false ;
-			this._obj.itemArr[crntIdx].selected = true ;
-			this._elem.newBtn.textContent = this._obj.itemArr[crntIdx].title ;
-			this._opt.prevIdx = crntIdx ;
-			if( this._elem.newWrapper.classList.contains( this._opt.activeClass ) ) this.selectToggle() ;
+			this.crntSelect = this.itemArr[crntIdx] ;
+			this.originSelect.selectedIndex = crntIdx ;
+			this.itemArr[this.prevIdx].selected = false ;
+			this.itemArr[crntIdx].selected = true ;
+			this.newBtn.textContent = this.itemArr[crntIdx].title ;
+			this.prevIdx = crntIdx ;
+			if( this.newWrapper.classList.contains( this.activeClass ) ) this.selectToggle() ;
 
 		}
 
 	}
 
 	(() => {
-		let slctBoxs = null ;
+
+		/*
+			select.length 로 체크 해서 하나일 경우 여러개 일경우 구분해줄려고 했는데
+			select 요소가 하나일 경우 length 사용 시 option 갯수를 가져오기 때문에 항상 1보다 크게 나온다.
+		 */
+		if ( target[0].nodeName == 'OPTION' ) {
+
+			new SelectMenuCustom( target, opts ) ;
+
+		} else {
+
+			[].forEach.call( target , slctBox => {
+				console.log('ininini');
+				new SelectMenuCustom( slctBox, opts ) ;
+			}) ;
+
+		}
+
+		// console.log( 'option :', option ) ;
+		// console.log( 'opts type :', typeof opts ) ;
+
+		// new SelectMenuCustom( slctBoxs ) ;
+
+
+		/*for( let i=0; i<opts.length; i+=1 ){
+			console.log(i, opts[i]);
+			if( /HTML/.test( {}.toString.call( opts[i] ) ) ){
+				slctBoxs = opts[i] ;
+			}else{
+				option = opts[i] ;
+			}
+		}
+
+		console.log( 'slctBoxs :', slctBoxs )
+		console.log( 'option :', option )
+
+
 		if ( opts ) {
 
-			console.log( 'sure' ) ;
+			console.log( 'sure!!!!!!!!!!!!!!!!!' ) ;
 			new SelectMenuCustom( opts ) ;
 
 		} else {
 
-			console.log( 'not' ) ;
+			console.log( 'not!!!!!!!!!!!!!!!!!' ) ;
 			slctBoxs = document.querySelectorAll( 'select' ) ;
 			[].forEach.call( slctBoxs , slctBox => {
 				new SelectMenuCustom( slctBox ) ;
 			}) ;
 
-		}
+		}*/
+
 	})() ;
+
 } ;
 
 
@@ -141,13 +185,48 @@ window.addEventListener('load', function(){
 	// let slt01 = new SelectMenuCustom( document.querySelector('.wrap_slt > select') ) ;
 	// let slt02 = new SelectMenuCustom( document.querySelector('.select02 > select') ) ;
 
-	// let slt02 = new SelectMenuCustom( document.querySelectorAll('.custom_select') ) ;
 
-	// SelectMenuCustomModule( document.querySelector('.wrap_slt > select') ) ;
-	SelectMenuCustomModule() ;
+	// SelectMenuCustomModule( document.querySelector('.select02 > select') ) ;
+	// SelectMenuCustomModule() ;
+
+
+	// SelectMenuCustomModule( document.querySelectorAll('.custom_select') ) ;
+	let sltBoxAll = document.querySelectorAll('select') ;
+	let sltBox01 = document.querySelector('select') ;
+	let sltBox02 = document.querySelectorAll('.select02 > select') ;
+
+	// console.log( 'sltBox01 :', sltBox01, isArrayLike( sltBox01 )) ;
+	// SelectMenuCustomModule( sltBox ) ;
+	// SelectMenuCustomModule({
+	// 	dir : 'up' ,
+	// }) ;
+	// SelectMenuCustomModule( sltBox01 ) ;
+	// SelectMenuCustomModule( sltBoxAll ) ;
+	SelectMenuCustomModule( sltBox01, { dir : 'up' } ) ;
+	SelectMenuCustomModule( sltBox02, { dir : 'down' } ) ;
+
+
+	/*
+
+		두가지 인자를 넘길 수 있다.
+		1. DOM
+		2. 옵션 객체
+
+		인자 경우의 수
+		1. DOM만 넘길경우
+		2. DOM, 옵션 모두 넘길경우
+		3. 옵션만 넘길경우
+
+
+		DOM을 넘기지 않으면 모든 select박스에 적용한다.
+		DOM을 넘기면 해당 select박스들만 찾아서 적용한다.
+		두번째 인자로 옵션을 넘길수 있다.
+	*/
 
 
 	// { 옵션 넘긴다 }
 
 
 }) ;
+
+
